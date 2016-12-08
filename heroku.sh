d572bad5b50ab5a3d9733c640c6c007ed2cfda60
Dhruv Apte <dhruvgirishapte@gmail.com>
2016-12-08 23:50:53 +0530

    Add files via upload

--- heroku.sh
+++ heroku.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 export INTEGRATE_SOCKETIO=false
-# socketio has problems with celery "blocking" tasks
-# also socketio is not used in a celery task so no problem to turn it off
+#socketio has problems with celery blocking the taks
+#turning socketio off  as it's not used in a celery task
 celery worker -A app.celery --loglevel=info &
 unset INTEGRATE_SOCKETIO
 gunicorn app:app --worker-class eventlet -w 1
-# if not running on free dyno
-# define a separate worker and scale
-# https://devcenter.heroku.com/articles/celery-heroku
+#define a separate worker and scale if not running on free dyno
